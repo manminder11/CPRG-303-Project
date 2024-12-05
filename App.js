55e761b7d4497e51f3899b4beec6015d156ea025
Manminder <ManminderSingh@edu.sait.ca>
2024-12-05 05:01:53 -0700

    one domne

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet } from 'react-native'
 import HomePage from './home'
 import MinePage from './Mine'
 import NavigationPage from './Navigation'
+import search from './search'
 
 export default function App() {
     const Tab = createBottomTabNavigator()
@@ -12,6 +13,7 @@ export default function App() {
     return (
         <NavigationContainer >
             <Tab.Navigator>
+                <Tab.Screen name='Search' component={search} />
                 <Tab.Screen name='Home' component={HomePage} />
                 <Tab.Screen name='Navi' component={NavigationPage} />
                 <Tab.Screen name='Mine' component={MinePage} />
